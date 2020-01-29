22b4a53e7bd6bdc5c69a2de8dd8b7bb944848198
smeerws <wilfried.gruber@htl-salzburg.ac.at>
2020-01-29 11:32:37 +0100

    Add simplistic Character Controller

---
+++ Assets/MyGame/Scripts/WormsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float jumpForce;
+    public float moveForce;
+
+    //public KeyCode test;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 force = new Vector3(0,jumpForce,0);
+            rb.AddForce(force);
+            Debug.Log("Taste ist gedrückt!");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 force = new Vector3(moveForce, 0, 0);
+            rb.AddForce(force);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 force = new Vector3(-moveForce, 0, 0);
+            rb.AddForce(force);
+        }
+    }
+}
